56c8d7dc5ef0c898cfcbb498f283f4227eea60ec
VincetCampos <vcamposs1@miumg.edu.gt>
2021-02-22 13:50:20 -0600

    El primer commit en C#

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HolaMundoC_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mensaje;
+            mensaje = "Hola Mundo desde C#";
+            Console.WriteLine(mensaje);
+        }
+    }
+}
